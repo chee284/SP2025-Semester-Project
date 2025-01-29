f4b55dc03b48574cfbec5eefaa76d91820559138
evan chee <chee@wustl.edu>
2025-01-28 18:47:38 -0600

    Starter react-vite with JS and TailwindCSS

---
+++ ski-slopes/src/app/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className="bg-amber-800">3D Topology</p>
+      </div>
+    </>
+  )
+}
+
+export default App
