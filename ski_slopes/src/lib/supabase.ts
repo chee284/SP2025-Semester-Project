f22ab98f2b19167a3bd5135108738973eaf7ad68
nathanreyes32 <n.e.reyes@wustl.edu>
2025-04-20 13:19:53 -0500

    works for both localhosta and 127.0.0.1

--- ski_slopes/src/lib/supabase.ts
+++ ski_slopes/src/lib/supabase.ts
@@ -1,7 +1,9 @@
 // src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "http://127.0.0.1:54321";
+const SUPABASE_URL = window.location.hostname === '127.0.0.1' 
+    ? "http://127.0.0.1:54321"
+    : "http://localhost:54321";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
